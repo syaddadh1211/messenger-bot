5e41178f19508d3c05e03aab9d71db8052e6fa5e
syaddadh1211 <bahalwans@gmail.com>
2021-12-19 00:43:32 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -90,7 +90,7 @@ app.get("/", (req, res) => {
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
   let response;
-
+  console.log("handlemessage : " + received_message.text);
   // Check if the message contains text
   if (received_message.text) {
     // Create the payload for a basic text message
