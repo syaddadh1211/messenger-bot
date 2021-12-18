32a21d3e09d1a1ab8d5f7acefcfab6db0962b95a
syaddadh1211 <bahalwans@gmail.com>
2021-12-18 21:49:14 +0700

    update server.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,7 +1,7 @@
 require("dotenv").config({ path: "./src/.env" });
 import express from "express";
 import homepageController from "../controllers/homepageController";
-import chatBotController from "../controllers/chatBotController";
+// import chatBotController from "../controllers/chatBotController";
 
 const MY_VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
 let router = express.Router();
